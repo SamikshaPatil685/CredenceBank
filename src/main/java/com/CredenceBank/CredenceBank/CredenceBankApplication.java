e18be08429341420bd5d709bdfa8e6e51b3f5dcb
Samiksha Patil <samikshapatil685@gmail.com>
2026-05-07 17:24:41 +0530

    Connected to Database

---
+++ src/main/java/com/CredenceBank/CredenceBank/CredenceBankApplication.java
@@ -0,0 +1,13 @@
+package com.CredenceBank.CredenceBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredenceBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredenceBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CredenceBank/CredenceBank/CredenceBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CredenceBank.CredenceBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredenceBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
